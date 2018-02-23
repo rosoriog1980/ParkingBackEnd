1c8bf11d792c2dd77d91c759e5d02f2653875da2
PSLCOL\rosoriog <rosoriog@psl.com.co>
2018-02-23 15:15:51 -0500

    Authentication and authorization

--- src/server/Api/user/routing.js
+++ src/server/Api/user/routing.js
@@ -1,12 +1,34 @@
 const router = require('express').Router();
+const User = require('./user.model');
 const { getUsers, createUser, deleteUser, newVehicle, removeVehicle, loginUser, apiValidateToken } = require('./user.controller');
 
-router.get('/', getUsers);
-router.post('/', createUser);
-router.post('/vehicle', newVehicle);
 router.post('/auth', loginUser);
 router.post('/validAuth', apiValidateToken)
+router.post('/', createUser);
+
+router.get('/', isAuthenticated, getUsers);
+router.post('/vehicle', newVehicle);
 router.delete('/', deleteUser);
 router.delete('/vehicle', removeVehicle);
+
+
+function isAuthenticated(req, res, next){
+    const Token = req.get('Token');
+    
+    User.findOne({loginToken: Token})
+    .then(user => {
+        if (user != null) {
+            return next();    
+        } else {
+            res.status(404);
+            return res.send('Bad Request.');
+        }
+    })
+    .catch(err => {
+        res.status(404);
+        return res.send('Bad Request.');
+    });
+}
+
 
 module.exports = router; 
